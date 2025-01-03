0e973cfb9e02f227cbe32b50ceac551e1626450e
Ronit Gupta <ronit1808@gmail.com>
2025-01-03 15:45:15 +0530

    changes to settings

--- Backend/Blogmagic_project/settings.py
+++ Backend/Blogmagic_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.207.100.58']
 
 
 # Application definition
