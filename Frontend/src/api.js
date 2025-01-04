90ff50c3ffd608f5ab2951196bc932609a483cb5
Ronit Gupta <ronit1808@gmail.com>
2025-01-04 20:01:26 +0530

    added backend url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://127.0.0.1:8000/api/'
+const baseURL = 'https://blogmagic.onrender.com/api';
 const api = axios.create({
   baseURL: baseURL, 
   headers: {
